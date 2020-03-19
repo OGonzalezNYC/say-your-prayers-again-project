0a93a0ac2f09f5cab4d5b63ca317efe6777aa497
github username <github email address>
2020-03-19 12:42:20 -0400

    Just in time for Dave.

--- say-your-prayers-again-project-api/db/seeds.rb
+++ say-your-prayers-again-project-api/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+dave = User.create(username: 'Dave', password: 'password')
+mary = User.create(username: 'Mary', password: 'password')
+omar = User.create(username: 'Omar', password: 'password')
+
+Prayer.create(title: 'My mom has cancer', body: 'Lord, please heal her.', user: omar)
+Prayer.create(title: "I am going to propose to my gf.", body: 'God, please make her my wife.', user: omar)
+Prayer.create(title: 'Election 2020', body: 'I sure would appreciate a shake-up of some sort.', user: mary)
+Prayer.create(title: 'Graduation', body: 'Father God, please help me to achieve it.', user: dave)
